62af989907af13312ee31cb82015abcfd4586751
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-02-27 21:47:13 +0000

    final edits

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const request = store.put({ value: content });
   const result = await request;
   console.log('data saved to the database', result);
 };
@@ -32,7 +32,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('got all data', result);
-  return result;
+  // return result;
 };
 
 initdb();
